134eb34232a49171bd263459eca3c7a53727cf71
vladosik_py <vbsmulskaya@gmail.com>
2023-01-07 12:22:23 +0300

    hw_19

--- service/user.py
+++ service/user.py
@@ -41,13 +41,8 @@ class UserService:
 
         return base64.b64encode(hashed_password)
 
-    def compare_passwords(self, password_hash, other_password) -> bool:
+    def compare_password(self, password_hash, other_password) -> bool:
         return hmac.compare_digest(
-            base64.b64encode(password_hash),
-            hashlib.pbkdf2_hmac(
-                'sha256',
-                other_password.encode("utf-8"),
-                PWD_HASH_SALT,
-                PWD_HASH_ITERATIONS
-            )
+            password_hash,
+            self.password_hash(other_password)
         )
